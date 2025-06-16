fbf48ee23021c45dd3499f7dbd86be2916bc68a4
Cyber-Syntax <115875369+Cyber-Syntax@users.noreply.github.com>
2025-06-16 18:14:42 +0300

    feat: add dev tools and treesitter-context plugin
    Add gparted, kernel-tools, and kdiskmark to dev packages.
    Add nvim-treesitter-context plugin for neovim

--- dot-config/nvim/lua/plugins/extras.lua
+++ dot-config/nvim/lua/plugins/extras.lua
@@ -7,8 +7,12 @@ return {
   { import = "lazyvim.plugins.extras.editor.harpoon2" },
   { import = "lazyvim.plugins.extras.editor.mini-diff" },
   { import = "lazyvim.plugins.extras.ai.copilot-chat" },
+  -- octo github integration, able to comment isue, send PR
   { import = "lazyvim.plugins.extras.util.octo" },
   { import = "lazyvim.plugins.extras.util.dot" },
+  -- NOTE: This make class, function, try:, if command is seen on the top of the file
+  -- It would helpful for: "am I on the try block or if block, am I on the folder class or zip class"
+  { import = "lazyvim.plugins.extras.ui.treesitter-context" },
   --FIX: prettier cause formatting issues on .jsonc files
   -- { import = "lazyvim.plugins.extras.formatting.prettier" },
 }
