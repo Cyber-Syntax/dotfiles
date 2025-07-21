5f4644909484afa4dde632435c35589747b5eb0f
Cyber-Syntax <115875369+Cyber-Syntax@users.noreply.github.com>
2025-07-21 11:16:07 +0300

    hack: auto-cpufreq for laptop

--- dot-config/nvim/lua/plugins/theme.lua
+++ dot-config/nvim/lua/plugins/theme.lua
@@ -3,16 +3,16 @@ return {
     "LazyVim/LazyVim",
     opts = {
       -- old: github_dark,
-      colorscheme = "darcula-dark",
+      colorscheme = "github_dark",
     },
   },
 
-  {
-    "xiantang/darcula-dark.nvim",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-    },
-  },
+  -- {
+  --   "xiantang/darcula-dark.nvim",
+  --   dependencies = {
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+  -- },
   -- -- base16
   -- {
   --   "RRethy/base16-nvim",
@@ -25,16 +25,16 @@ return {
   --     require("nordic").load()
   --   end,
   -- },
-  -- {
-  --   "projekt0n/github-nvim-theme",
-  --   name = "github-theme",
-  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  --   priority = 1000, -- make sure to load this before all the other start plugins
-  --   config = function()
-  --     require("github-theme").setup({
-  --       -- ...
-  --     })
-  --     vim.cmd("colorscheme github_dark")
-  --   end,
-  -- },
+  {
+    "projekt0n/github-nvim-theme",
+    name = "github-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+      vim.cmd("colorscheme github_dark")
+    end,
+  },
 }
--- dot-config/qtile/scripts/autostart.sh
+++ dot-config/qtile/scripts/autostart.sh
@@ -8,7 +8,7 @@ picom -b & # compositor
 numlockx on &
 setxkbmap tr &
 gammastep & # redshift alternative (works wayland and xorg)
-python3 /home/developer/Documents/repository/WallpaperChanger/main.py &
+python3 /home/developer/Documents/my-repos/WallpaperChanger/main.py &
 flatpak run io.github.martchus.syncthingtray & # syncthing tray
 
 # Determine which machine-specific setup to run
@@ -16,12 +16,11 @@ HOSTNAME=$(hostname)
 #FIXME: seems like hostname is not detected correctly
 # which it is try to run on laptophostname named fedora-laptop
 if [ "$HOSTNAME" = "fedora" ]; then
-  xset -dpms &                                                     # disable power management (DPMS) causes screen to sleep after 10 minutes
-  xset s off &                                                     # disable screen saver
-  ~/.local/share/myunicorn/super-productivity.AppImage &           # task app new location
-  sh /home/developer/Documents/scripts/screenloyout/asus_only.sh & # My screen layout scripts
-  keepassxc &                                                      # password manager
-  ckb-next --background &                                          # corsair keyboard manager
+  xset -dpms &                                           # disable power management (DPMS) causes screen to sleep after 10 minutes
+  xset s off &                                           # disable screen saver
+  ~/.local/share/myunicorn/super-productivity.AppImage & # task app new location
+  keepassxc &                                            # password manager
+  ckb-next --background &                                # corsair keyboard manager
   # else
   #   # For laptop (hostname = "fedoraLaptop") or any other machine
   #   # Laptop-specific applications will go here when ready
