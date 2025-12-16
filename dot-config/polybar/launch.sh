8a515d0b4c6fb8695dbe73393bfafdbd38735a18
Cyber-Syntax <115875369+Cyber-Syntax@users.noreply.github.com>
2025-12-16 15:26:56 +0300

    fix: polybar two machine error
    Added intact hostname command usage to solve issue on laptop polybar
    wasn't executed

--- dot-config/polybar/launch.sh
+++ dot-config/polybar/launch.sh
@@ -13,16 +13,10 @@ if [ "$hostname" = "fedora" ]; then
   echo "Launching polybar for desktop..."
   polybar desktop 2>&1 | tee -a ~/.config/polybar/logs/desktop.log &
   disown
-else
+elif [ "$hostname" = "developer-laptop" ]; then
   echo "Launching polybar for laptop..."
   polybar laptop 2>&1 | tee -a ~/.config/polybar/logs/laptop.log &
   disown
+else
+  echo "No polybar configuration for this host."
 fi
-
-# polybar desktop 2>&1 | tee -a ~/.config/polybar/logs/main.log &
-# disown
-
-# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log /tmp/polybar3.log
-# polybar mybar 2>&1 | tee -a ~/tmp/polybar1.log & disown
-# polybar mybar2 2>&1 | tee -a /tmp/polybar2.log & disown
-# polybar mybar3 2>&1 | tee -a /tmp/polybar3.log & disown
