78c7fdcdd259fd5a16247f748e4e3dd6141545a7
Cyber-Syntax <115875369+Cyber-Syntax@users.noreply.github.com>
2025-12-22 22:20:11 +0300

    refactor: update rofi and simplify
    - Update powermenu and launcher to follow best security practices provided by shellcheck
    - Remove unused styles and themes from rofi

---
+++ dot-config/rofi/README.md
@@ -0,0 +1,36 @@
+# Rofi themes
+>
+> [!NOTE]
+> Main source: <https://github.com/adi1090x/rofi>
+>
+> I modified them to make more secure bash scripts and simplified some themes.
+
+## Power Menu - Simplified Type 6 version
+
+- Uptime and Power options only
+
+```bash
+*****************************************
+
+-            Uptime section             *
+-            Suspend  Poweroff          *   
+-            Lock     Restart           *
+-            Logout                     *
+
+*****************************************
+```
+
+## Launcher Menu - Simplified Type 6 version
+
+- Only app launcher with search bar
+
+```bash
+*****************************************
+
+-            Search Bar                 *
+-            Apps                       *   
+-                                       *
+-                                       *
+
+*****************************************
+```
--- dot-config/rofi/powermenu/type-6/powermenu.sh
+++ dot-config/rofi/powermenu/type-6/powermenu.sh
@@ -2,24 +2,23 @@
 
 ## Author : Aditya Shakya (adi1090x)
 ## Github : @adi1090x
+##
+## Modified by : Cyber-Syntax
 #
 ## Rofi   : Power Menu
 #
 ## Available Styles
 #
-## style-1   style-2   style-3   style-4   style-5
+## style-1
 
 # Current Theme
 dir="$HOME/.config/rofi/powermenu/type-6"
 theme='style-1'
 
 # CMDs
-lastlogin="`last $USER | head -n1 | tr -s ' ' | cut -d' ' -f5,6,7`"
-uptime="`uptime -p | sed -e 's/up //g'`"
-host=`hostname`
+uptime="$(uptime -p | sed -e 's/up //g')"
 
 # Options
-hibernate=''
 shutdown=''
 reboot=''
 lock=''
@@ -31,9 +30,8 @@ no=''
 # Rofi CMD
 rofi_cmd() {
 	rofi -dmenu \
-		-p " $USER@$host" \
 		-mesg " Uptime: $uptime" \
-		-theme ${dir}/${theme}.rasi
+		-theme "${dir}/${theme}.rasi"
 }
 
 # Confirmation CMD
@@ -46,7 +44,7 @@ confirm_cmd() {
 		-dmenu \
 		-p 'Confirmation' \
 		-mesg 'Are you Sure?' \
-		-theme ${dir}/${theme}.rasi
+		-theme "${dir}/${theme}.rasi"
 }
 
 # Ask for confirmation
@@ -56,7 +54,7 @@ confirm_exit() {
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$lock\n$suspend\n$logout\n$hibernate\n$reboot\n$shutdown" | rofi_cmd
+	echo -e "$lock\n$suspend\n$logout\n$shutdown\n$reboot" | rofi_cmd
 }
 
 # Execute Command
@@ -70,10 +68,7 @@ run_cmd() {
 		elif [[ $1 == '--hibernate' ]]; then
 			systemctl hibernate
 		elif [[ $1 == '--suspend' ]]; then
-			#mpc -q pause
-			#amixer set Master mute
-			# Pause music
-			playerctl pause
+			playerctl pause   
 			systemctl suspend
 		elif [[ $1 == '--logout' ]]; then
 			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
@@ -102,26 +97,23 @@ run_cmd() {
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
-    $shutdown)
+    "$shutdown")
 		run_cmd --shutdown
         ;;
-    $reboot)
+    "$reboot")
 		run_cmd --reboot
         ;;
-    $hibernate)
-		run_cmd --hibernate
-        ;;
-    $lock)
+    "$lock")
 		if [[ -x '/usr/bin/betterlockscreen' ]]; then
 			betterlockscreen -l
 		elif [[ -x '/usr/bin/i3lock' ]]; then
 			i3lock
 		fi
         ;;
-    $suspend)
+    "$suspend")
 		run_cmd --suspend
         ;;
-    $logout)
+    "$logout")
 		run_cmd --logout
         ;;
 esac
