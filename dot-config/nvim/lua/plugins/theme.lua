916909bb50bdc1acc17eaf26bd26cae0ea1a3155
Cyber-Syntax <115875369+Cyber-Syntax@users.noreply.github.com>
2026-04-28 19:43:24 +0300

    feat: add ty/ruff for better python setup

--- dot-config/nvim/lua/plugins/theme.lua
+++ dot-config/nvim/lua/plugins/theme.lua
@@ -8,6 +8,56 @@ return {
   -- },
 
   -- {
+  --   "loctvl842/monokai-pro.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("monokai-pro").setup({
+  --       transparent_background = false,
+  --       terminal_colors = true,
+  --       devicons = true,
+  --       styles = {
+  --         comment = { italic = true },
+  --         keyword = { italic = true },
+  --         type = { italic = true },
+  --         storageclass = { italic = false },
+  --         structure = { italic = true },
+  --         parameter = { italic = true },
+  --         annotation = { italic = true },
+  --         tag_attribute = { italic = true },
+  --       },
+  --       filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+  --       day_night = {
+  --         enable = false,
+  --         day_filter = "pro",
+  --         night_filter = "spectrum",
+  --       },
+  --       inc_search = "background", -- underline | background
+  --       background_clear = {
+  --         "toggleterm",
+  --         "telescope",
+  --         "renamer",
+  --         "notify",
+  --       },
+  --       plugins = {
+  --         bufferline = {
+  --           underline_selected = false,
+  --           underline_visible = false,
+  --           underline_fill = false,
+  --           bold = true,
+  --         },
+  --         indent_blankline = {
+  --           context_highlight = "pro", -- default | pro
+  --           context_start_underline = false,
+  --         },
+  --       },
+  --     })
+  --
+  --     vim.cmd.colorscheme("monokai-pro")
+  --   end,
+  -- },
+
+  -- {
   --   "folke/tokyonight.nvim",
   --   lazy = false,
   --   priority = 1000,
